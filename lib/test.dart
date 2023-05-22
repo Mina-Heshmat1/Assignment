3a63c471e5726c64e2004d4053a9aa7b159fe5b4
Mina.Heshmat <minaheshmat33@gmail.com>
2023-05-22 17:36:26 +0200

    4

---
+++ lib/test.dart
@@ -0,0 +1,17 @@
+
+
+/*
+void main() {
+  var allNames=sumNames();
+  print(allNames+'kjlkh');
+}
+String sumNames(){
+  var myName=['mina','heshmat','maske','mawede'];
+  var reng='';
+  for(final m in myName){
+    reng+=m;
+  }
+  print(reng);
+  return reng ;
+}
+*/
